40b41cc147fa63238cbb1da2632ebb0af2b8a93a
kevinseq1 <kevinseq1@gmail.com>
2018-08-30 14:25:07 -0400

    Add files via upload

---
+++ It's All Good/It's All Good/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  It's All Good
+//
+//  Created by KS on 8/22/18.
+//  Copyright © 2018 KS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
